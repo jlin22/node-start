66797a59f9afdb2d8647ff8efecf80e1206f0fa4
John Lin <jlin1674@gmail.com>
2019-01-09 14:21:38 -0500

    exported module

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var http = require("http");
 
-http.createServer(function (request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
-}).listen(3000);
+function start() {
+	function onRequest(request, response) {
+		console.log("Request recieved");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello, world!");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(3000);
+	console.log("Server started");
+}
+
+start();
+
+exports.start = start;
+
+
