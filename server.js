58529580b7a955ea2a834b46ae5394f56b8cbafc
John Lin <jlin1674@gmail.com>
2019-01-09 14:25:40 -0500

    parse url & index.js using server.js module

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require("http");
+var url = require("url");
 
 function start() {
 	function onRequest(request, response) {
-		console.log("Request recieved");
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " recieved");
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello, world!");
 		response.end();
@@ -11,8 +13,6 @@ function start() {
 	http.createServer(onRequest).listen(3000);
 	console.log("Server started");
 }
-
-start();
 
 exports.start = start;
 
