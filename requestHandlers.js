7cd2e3869a32c9d9865319cf524294468fca0d0b
John Lin <jlin1674@gmail.com>
2019-01-09 16:55:20 -0500

    finished basic nodejs

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,14 @@
 var execute = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
 	console.log("Request handler 'start' was called");
 
 	var body = '<!DOCTYPE html>' +
 		'<html>' +
+		'<head>' + 
+		'<meta charset="utf-8" />' + 
+		'</head>' + 
 		'<body>' +
 		'<form action="/upload" method="post">' +
 		'<textarea name="text" rows="20" cols="60"></textarea>' + 
@@ -17,10 +21,10 @@ function start(response) {
 	response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
 	console.log("Request handler 'upload' was called");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Upload");
+	response.write("You've sent the text: " + querystring.parse(postData).text);
 	response.end();
 }
 
--- server.js
+++ server.js
@@ -3,10 +3,20 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " recieved");
 
-		route(handle, pathname, response);
+		request.setEncoding("utf8");
+
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Recieved post data chunk '" + postDataChunk + "'");
+		});
+
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
 	}
 
 	http.createServer(onRequest).listen(3000);
